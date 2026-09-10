532b86640747c7a7b1710a00d69081b3c9aaccf4
zxngxtsu <zxngxtsu.hollow@gmail.com>
2026-09-11 01:39:54 +0300

    Refactor CRUD into package

---
+++ crud/__init__.py
@@ -0,0 +1,16 @@
+from .products import (
+    get_products,
+    get_product,
+    create_product,
+    update_product,
+    delete_product,
+)
+
+
+__all__ = [
+    "get_products",
+    "get_product",
+    "create_product",
+    "update_product",
+    "delete_product",
+]
